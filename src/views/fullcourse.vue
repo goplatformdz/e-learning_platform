7996e9926a14da21e4b7d38ed621b26338f2a3d3
SamiRZL <rezalsami2001@gmail.com>
2023-09-30 17:23:54 +0100

    last commit

--- src/views/fullcourse.vue
+++ src/views/fullcourse.vue
@@ -13,10 +13,10 @@
         <h4>{{ fetchedLesson.lessonName }}</h4>
       </div>
       <div>
-        <iframe width="386" height="687" src="https://www.youtube.com/embed/iqfOH7zUWx0"
-          title="Do you have a Girlfriend? 😂🔥 - Steve Harvey" frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen></iframe>
+
+
+        <iframe :src="fetchedLesson.video" width="640" height="360" frameborder="0"
+          allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
 
 
       </div>
